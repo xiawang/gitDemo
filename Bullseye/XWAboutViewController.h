ce32cba65ddd61f459c9a9e8a41ddfe61b5ef7b9
王小天 <wxt1994@gmail.com>
2014-09-05 13:51:22 -0700

    Completed whole game. Fixed some of the bugs.

---
+++ Bullseye/XWAboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  XWAboutViewController.h
+//  Bullseye
+//
+//  Created by 王小天 on 14-9-5.
+//  Copyright (c) 2014年 Wang Xiaotian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XWAboutViewController : UIViewController
+- (IBAction)close:(id)sender;
+
+@end
